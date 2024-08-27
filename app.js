dac4794c67a248569f173342443dada3a2c0b5ee
Sushil Kumar <sushilkumar567895@gmail.com>
2024-08-27 13:16:50 +0530

    files added

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const apiKey = "17d705c3dde0852cbc7e776fb8fbc222";
-const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+let apiKey = "17d705c3dde0852cbc7e776fb8fbc222";
+let apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const searchBox = document.querySelector(".search input");
-const searchBtn = document.querySelector(".search button");
-
-const weatherIcon = document.querySelector(".weather-icon");
+let searchBox = document.querySelector(".search input");
+let searchBtn = document.querySelector(".search button");
+let weatherIcon = document.querySelector(".weather-icon");
 
 const checkWeather = async (city) => {
-  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+  let response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   } else {
     let data = await response.json();
-
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML =
       Math.round(data.main.temp) + "°c";
